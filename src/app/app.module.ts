61d1e05f6294bac00e3b7c8384a3b36a53d575d1
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-02 16:25:29 -0300

    feat: adicionando os components do Hero-Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HeroDetailComponent } from './hero-datail/hero-detail.component';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,7 +10,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
