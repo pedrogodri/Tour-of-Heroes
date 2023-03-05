63291ca972c8bdd150bc44e3f410bd281ca7e875
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-05 11:15:40 -0300

    feat: adicionando o Observable

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,13 +1,15 @@
 import { Hero } from './../models/hero.model';
 import { Injectable } from '@angular/core';
 import { HEROES } from './../mock-heroes';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
